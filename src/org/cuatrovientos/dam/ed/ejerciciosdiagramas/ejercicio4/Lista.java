c5a4d764388ba94d38a5d1bdf1da6add55934368
Ivan <ivancarrillovela@gmail.com>
2025-02-26 15:15:41 +0100

    Ejercicio 4 terminado

--- src/org/cuatrovientos/dam/ed/ejerciciosdiagramas/ejercicio4/Lista.java
+++ src/org/cuatrovientos/dam/ed/ejerciciosdiagramas/ejercicio4/Lista.java
@@ -1,5 +1,26 @@
 package org.cuatrovientos.dam.ed.ejerciciosdiagramas.ejercicio4;
 
-public class Lista {
+import java.util.ArrayList;
 
+public class Lista {
+	
+	private int id;
+	private ArrayList<Producto> productos=new ArrayList<>();
+	
+	public Lista() {
+		super();
+	}
+	
+	public void anadirProducto(Producto producto) {
+		productos.add(producto);
+	}
+	
+	public void tacharProducto(Producto producto) {
+		productos.remove(producto);
+	}
+
+	public String verLista() {
+		return "Lista [id=" + id + ", productos=" + productos + "]";
+	}
+	
 }
--- src/org/cuatrovientos/dam/ed/ejerciciosdiagramas/ejercicio4/Producto.java
+++ src/org/cuatrovientos/dam/ed/ejerciciosdiagramas/ejercicio4/Producto.java
@@ -1,5 +1,22 @@
 package org.cuatrovientos.dam.ed.ejerciciosdiagramas.ejercicio4;
 
 public class Producto {
+	
+	private int ean;
+	private String nombre;
+	private double precio;
+	private int cantidad;
+	
+	public Producto() {
+		super();
+	}
+
+	public Producto(int ean, String nombre, double precio, int cantidad) {
+		super();
+		this.ean = ean;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.cantidad = cantidad;
+	}
 
 }
